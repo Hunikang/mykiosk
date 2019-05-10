5cc8d3ce79012273557a82f523a1fd84a29993ca
hoon <snowleopard0902@naver.com>
2019-05-10 11:09:10 +0900

    05101109

--- mykiosk/js/order.js
+++ mykiosk/js/order.js
@@ -1,11 +1,28 @@
 
 
-const coffeeList = [
-    ''
-]
+function loadJson(callback){ //에이젝스
 
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'CoffeeData.json', true);
+    xhr.onreadystatechange = function(){
+        if(xhr.status == 200 && xhr.readyState == 4){
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send();
+}
 
+function init(){ //에이젝스호출
 
+    loadJson(function(data){
+
+        var jsonData = JSON.parse(data);
+        for(var n in jsonData){
+            console.log(jsonData[n].name);
+        }
+
+    });
+}
 
 const Product = {
     template: `
@@ -20,48 +37,66 @@ const Product = {
     `
 }
 
-var a = new Vue({
+var slide1 = new Vue({
     el: '.slide1',
     components: {
         'show': Product,
     }
 });
 
-var b = new Vue({
+var slide2 = new Vue({
     el: '.slide2',
     components: {
         'show': Product,
     }
 });
 
-var c = new Vue({
+var slide3 = new Vue({
     el: '.slide3',
     components: {
         'show': Product,
     }
 });
 
-function initProduct() {
-    const img = [];
-    const productLength = document.querySelectorAll('.coffeeMenu');
-    const div = "<img class='coffeeImg'src='#'>"
 
+
+
+function initProduct() { //상품목록 업데이트
+    const productLength = document.querySelectorAll('.coffeeMenu');
+    const coffeeImgBox = "<img class='coffeeImg'src='#'>"
+    const coffeeNameBox = "<span class='coffeeName'></span>";
+    const coffeePriceBox = "<span class='coffeePrice'></span>";
+    var jsonData;
+   
     for (let i = 0; i < productLength.length; i++) {
-        img[i] = './img/coffe' + i + '.png';
-        productLength[i].insertAdjacentHTML('beforeend', div);
+        productLength[i].insertAdjacentHTML('beforeend', coffeeImgBox);
+        productLength[i].insertAdjacentHTML('beforeend', coffeeNameBox);
+        productLength[i].insertAdjacentHTML('beforeend', coffeePriceBox)
     }
 
     const coffeeImg = document.querySelectorAll('.coffeeImg');
+    const coffeeName = document.querySelectorAll('.coffeeName');
+    const coffeePrice = document.querySelectorAll('.coffeePrice');
 
-    for (let i = 0; i < productLength.length; i++) {
-        coffeeImg[i].src = img[i];
-      
-    }
+    loadJson(function(data){
+        jsonData = JSON.parse(data);
+        var cnt = 0;
+        for(let i in jsonData){ // 이미지주소 , 이름 , 가격 뿌려주기
+            coffeeImg[cnt].src=jsonData[i].img;
+            coffeeName[cnt].innerHTML=jsonData[i].name;
+            coffeePrice[cnt].innerHTML=jsonData[i].price;
+            cnt++;
+            console.log(jsonData[i].img);
+        }
+        
+    });
+
+  
 }
 initProduct();
 
 
-function BtnEvent(btn) {
+function BtnEvent(btn) { //버튼 누를때 애니메이션
     const prevBtn = document.querySelector('#headerPrev');
     const NextBtn = document.querySelector('#headerNext');
 
