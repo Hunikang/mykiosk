1242df19da2f583d376b34bc3233453bb2855f86
hoon <snowleopard0902@naver.com>
2019-05-10 14:03:41 +0900

    05101401

--- mykiosk/js/order.js
+++ mykiosk/js/order.js
@@ -69,9 +69,9 @@ function initProduct() { //상품목록 업데이트
     var jsonData;
    
     for (let i = 0; i < productLength.length; i++) {
-        productLength[i].insertAdjacentHTML('beforeend', coffeeImgBox);
-        productLength[i].insertAdjacentHTML('beforeend', coffeeNameBox);
-        productLength[i].insertAdjacentHTML('beforeend', coffeePriceBox)
+        productLength[i].insertAdjacentHTML('beforeend', coffeeImgBox); //이미지
+        productLength[i].insertAdjacentHTML('beforeend', coffeeNameBox); // 이름
+        productLength[i].insertAdjacentHTML('beforeend', coffeePriceBox) // 가격
     }
 
     const coffeeImg = document.querySelectorAll('.coffeeImg');
@@ -82,7 +82,7 @@ function initProduct() { //상품목록 업데이트
     loadJson(function(data){
         jsonData = JSON.parse(data);
         var cnt = 0;
-        for(let i in jsonData){ // 이미지주소 , 이름 , 가격 뿌려주기
+        for(let i in jsonData){ // (이미지주소 , 이름 , 가격 가져오기 ) ( coffeeMenu 의 아이디를 CoffeeData의 name 키값으로함)
             coffeeImg[cnt].src=jsonData[i].img;
             coffeeName[cnt].innerHTML=jsonData[i].name;
             coffeePrice[cnt].innerHTML=jsonData[i].price;
@@ -127,23 +127,24 @@ BtnEvent();
 function order(){
     const Btn = document.querySelectorAll('.coffeeMenu');
     let coffeeName = [];
-    loadJson(function(data){
+    loadJson(function(data){ // 제이슨 데이터 추출
         const jsonData = JSON.parse(data);
         let cnt = 0;
         for(let n in jsonData){
-            coffeeName[cnt]=jsonData[n].name;
+            coffeeName[cnt]=jsonData[n].name; // coffeeName 배열에 커피[key].이름을 순서대로
             cnt++;
         }
     });
+
     for(let i=0; i<Btn.length; i++){
-        Btn[i].addEventListener('click',function(e){
-            if(!e.target.id){
-               alert(e.target.parentElement.id);
-               const tmp = e.target.parentElement.id;
-               const coffeeIndex = tmp.replace('coffee','');
-               localStorage.setItem(e.target.parentElement.id,coffeeName[coffeeIndex])
-            }else{
-                alert(e.target.id);
+        Btn[i].addEventListener('click',function(e){ //container 를 클릭했을떄
+            if(!e.target.id){ // 아이디가 없으면 ( 자식요소이면 )
+               
+               const tmp = e.target.parentElement.id; // 부모요소의 아이디(커피인덱스) 대입
+               const coffeeIndex = tmp.replace('coffee',''); // ex) coffee1 이면 coffee 삭제후 1을 coffeeName[숫자]로 사용하기위해 대입
+               localStorage.setItem(e.target.parentElement.id,coffeeName[coffeeIndex]) // coffeeName[인덱스] 로 클릭한 요소에 맞는 커피이름을 스토리지에 저장
+            
+            }else{ // 부모요소이면.
                 const tmp = e.target.id;
                 const coffeeIndex = tmp.replace('coffee','');
                 localStorage.setItem(e.target.id,coffeeName[coffeeIndex])
